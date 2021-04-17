2285ea25f426d4842824061ec2bcec5f0e9b908d
Mohamad Elnomrossie <m.elnomrossie@gmail.com>
2021-04-17 21:02:12 +0200

    cimm

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,5 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-
+ entry:'./src/client/index.js'// the starting point where webpack will start building the dependency tree from
 }
